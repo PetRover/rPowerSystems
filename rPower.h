1e543026a6f7e2e47612bd0bdd2e56ed29c9ca90
Bryce Cater <brycedcarter@gmail.com>
2015-08-31 11:37:29 -0400

    -started the declaration and implementation of rMotor functions -declared some required power system classes -updated CMakeLists to include .h files

--- rPower.h
+++ rPower.h
@@ -5,4 +5,34 @@
 #ifndef FIRMWARE_POWER_H
 #define FIRMWARE_POWER_H
 
+namespace  RVR
+{
+    enum RailName
+    {
+        RAIL12V0,
+        RAIL5V0,
+        RAIL3V3
+    };
+
+    // A class that allows control of the a individual power rail (net)
+    class PowerRail
+    {
+
+    };
+
+    // A class that manages the power rails for a system and provides power rail objects to users
+    class PowerManager
+    {
+    private:
+        static PowerRail* rail12v0;
+        static PowerRail* rail5v0;
+        static PowerRail* rail3v3;
+    public:
+        static PowerRail* getRail(RailName railName);
+
+    };
+
+
+}
+
 #endif //FIRMWARE_POWER_H
