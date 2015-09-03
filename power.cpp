ede47e68464968168ea964c0f35e0e2e36a8ea8e
Alyssa Bezreh <bezreh.a@husky.neu.edu>
2015-09-02 21:34:44 -0400

    Implementation of getRail function

--- power.cpp
+++ power.cpp
@@ -3,3 +3,19 @@
 //
 
 #include "rPower.h"
+
+namespace RVR
+{
+
+    PowerRail* PowerManager::getRail(RailName railName) {
+        switch (railName)
+        {
+            case RVR::RAIL12V0:
+                return rail12v0;
+            case RVR::RAIL5V0:
+                return rail5v0;
+            case RVR::RAIL3V3:
+                return rail3v3;
+        }
+    }
+}
